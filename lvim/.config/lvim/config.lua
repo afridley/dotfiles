41a6a0f524b592765925e93a86aa12eeba0664da
Andrew Fridley <andrew.fridley@nerdery.com>
2022-02-07 07:41:50 -0600

    update

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -222,8 +222,8 @@ lvim.plugins = {
   },
   {"navarasu/onedark.nvim"},
   -- "agreco/vim-citylights",
-"jordanbrauer/citylights.nvim",
-"tjdevries/colorbuddy.vim",
+  "jordanbrauer/citylights.nvim",
+  "tjdevries/colorbuddy.vim",
   -- Non essential
   {
     "ray-x/lsp_signature.nvim",
@@ -272,9 +272,21 @@ lvim.plugins = {
     "JoosepAlviste/nvim-ts-context-commentstring",
     event = "BufRead",
   },
+  {"lukas-reineke/indent-blankline.nvim"},
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
 --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
 -- }
+vim.g.indent_blankline_buftype_exclude = {'terminal'}
+vim.g.indent_blankline_filetype_exclude = {'man', 'help', 'startify', 'dashboard', 'packer', 'neogitstatus', 'markdown'}
+vim.g.indent_blankline_char = '▏'
+vim.g.indent_blankline_use_treesitter = true
+vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_show_current_context = true
+vim.g.indent_blankline_context_patterns = {
+    'class', 'return', 'function', 'method', '^if', '^while', 'jsx_element', '^for', '^object', '^table', 'block',
+    'arguments', 'if_statement', 'else_clause', 'jsx_element', 'jsx_self_closing_element', 'try_statement',
+    'catch_clause', 'import_statement', 'operation_type'
+}
