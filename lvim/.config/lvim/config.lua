a34955a30415bd037b08f3b46003e6948da0d859
Andrew Fridley <andrew.fridley@nerdery.com>
2022-07-27 13:31:00 -0500

    fix

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -19,13 +19,13 @@ lvim.colorscheme = "miramare"
 --     }
 -- }
 lvim.builtin.treesitter.autotag = {
-  enable = true,
+	enable = true,
 }
 lvim.builtin.treesitter.rainbow = {
-  enable = true,
+	enable = false,
 }
 lvim.builtin.treesitter.context_commentstring = {
-  enable = true,
+	enable = true,
 }
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
@@ -42,30 +42,30 @@ lvim.keys.normal_mode["<S-x>"] = ":bd<CR>"
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 local _, actions = pcall(require, "telescope.actions")
 lvim.builtin.telescope.defaults.mappings = {
-  -- for input mode
-  i = {
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-    ["<C-n>"] = actions.cycle_history_next,
-    ["<C-p>"] = actions.cycle_history_prev,
-  },
-  -- for normal mode
-  n = {
-    ["<C-j>"] = actions.move_selection_next,
-    ["<C-k>"] = actions.move_selection_previous,
-  },
+	-- for input mode
+	i = {
+		["<C-j>"] = actions.move_selection_next,
+		["<C-k>"] = actions.move_selection_previous,
+		["<C-n>"] = actions.cycle_history_next,
+		["<C-p>"] = actions.cycle_history_prev,
+	},
+	-- for normal mode
+	n = {
+		["<C-j>"] = actions.move_selection_next,
+		["<C-k>"] = actions.move_selection_previous,
+	},
 }
 
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = {
-  name = "+Trouble",
-  r = { "<cmd>Trouble lsp_references<cr>", "References" },
-  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-  d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
-  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+	name = "+Trouble",
+	r = { "<cmd>Trouble lsp_references<cr>", "References" },
+	f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+	d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+	q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+	l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+	w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
 }
 
 -- TODO: User Config for predefined plugins
@@ -82,17 +82,17 @@ vim.opt.expandtab = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "css",
-  "rust",
-  "java",
-  "yaml",
+	"bash",
+	"c",
+	"javascript",
+	"json",
+	"lua",
+	"python",
+	"typescript",
+	"css",
+	"rust",
+	"java",
+	"yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -124,61 +124,61 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
-  --   { command = "black", filetypes = { "python" } },
-  --   { command = "isort", filetypes = { "python" } },
-  -- {
-  --   -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-  --   command = "prettier",
-  --   ---@usage arguments to pass to the formatter
-  --   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-  --   extra_args = { "--print-with", "100" },
-  --   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-  --   filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-  -- },
-  {
-    command = "stylua",
-    filetypes = { "lua" },
-  },
+	--   { command = "black", filetypes = { "python" } },
+	--   { command = "isort", filetypes = { "python" } },
+	-- {
+	--   -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+	--   command = "prettier",
+	--   ---@usage arguments to pass to the formatter
+	--   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+	--   extra_args = { "--print-with", "100" },
+	--   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+	--   filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+	-- },
+	{
+		command = "stylua",
+		filetypes = { "lua" },
+	},
 
-  {
-    command = "eslint_d",
-    -- args = {"--fix-to-stdout"},
-    filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-  },
+	{
+		command = "eslint_d",
+		-- args = {"--fix-to-stdout"},
+		filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+	},
 })
 
 -- -- set additional linters
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
-  { command = "stylelint", filetypes = { "scss", "less", "css", "sass" }, args = { "--fix", "--stdin" } },
-  {
-    command = "eslint_d",
-    -- args = {"--fix-to-stdout"},
-    filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-  },
-  --   { command = "flake8", filetypes = { "python" } },
-  --   {
-  --     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-  --     command = "shellcheck",
-  --     ---@usage arguments to pass to the formatter
-  --     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-  --     extra_args = { "--severity", "warning" },
-  --   },
-  --   {
-  --     command = "codespell",
-  --     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-  --     filetypes = { "javascript", "python" },
-  --   },
+	{ command = "stylelint", filetypes = { "scss", "less", "css", "sass" }, args = { "--fix", "--stdin" } },
+	{
+		command = "eslint_d",
+		-- args = {"--fix-to-stdout"},
+		filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+	},
+	--   { command = "flake8", filetypes = { "python" } },
+	--   {
+	--     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+	--     command = "shellcheck",
+	--     ---@usage arguments to pass to the formatter
+	--     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+	--     extra_args = { "--severity", "warning" },
+	--   },
+	--   {
+	--     command = "codespell",
+	--     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+	--     filetypes = { "javascript", "python" },
+	--   },
 })
 
 -- Additional Plugins
 lvim.plugins = {
-  { "folke/tokyonight.nvim" },
-  {
-    "folke/trouble.nvim",
-    cmd = "TroubleToggle",
-  },
-  { "p00f/nvim-ts-rainbow" },
+	{ "folke/tokyonight.nvim" },
+	{
+		"folke/trouble.nvim",
+		cmd = "TroubleToggle",
+	},
+	{ "p00f/nvim-ts-rainbow" },
 
 	{ "p00f/nvim-ts-rainbow" },
 	{
@@ -237,15 +237,15 @@ lvim.plugins = {
 	{ "marko-cerovac/material.nvim" },
 	{
 		"rmehri01/onenord.nvim",
-		config = function()
-			require("onenord").setup({
-				styles = {
-					keywords = "italic",
-					functions = "italic",
-					comments = "italic",
-				},
-			})
-		end,
+		-- config = function()
+		-- 	require("onenord").setup({
+		-- 		styles = {
+		-- 			keywords = "italic",
+		-- 			functions = "italic",
+		-- 			comments = "italic",
+		-- 		},
+		-- 	})
+		-- end,
 	},
 	{ "frenzyexists/aquarium-vim" },
 	{ "rose-pine/neovim" },
