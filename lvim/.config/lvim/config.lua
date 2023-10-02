da46eb3418dec5126aa1085fe1479f1ef3497684
Andrew Fridley <andrew.fridley@nerdery.com>
2023-10-02 15:18:12 -0500

    update

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "kanagawa"
+lvim.colorscheme = "tokyonight"
 -- lvim.builtin.lualine = {
 --     active = false,
 --     options = {
--- scripts/.local/bin/lneo.sh
+++ scripts/.local/bin/lneo.sh
@@ -6,3 +6,4 @@ export LUNARVIM_CACHE_DIR="${LUNARVIM_CACHE_DIR:-"$HOME/.cache/lvim"}"
 
 # exec neovide --frame none --multigrid -- -u "$LUNARVIM_RUNTIME_DIR/lvim/init.lua" "$@"
 exec neovide --frame none -- -u "$LUNARVIM_RUNTIME_DIR/lvim/init.lua" "$@"
+# exec neovide -- -u "$LUNARVIM_RUNTIME_DIR/lvim/init.lua" "$@"
